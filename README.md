b71978d0ad4a39aaac4f6265920dcc80be018254
Mauc <tmauccc@gmail.com>
2023-09-05 13:09:33 +0200

    Add v1 of the projet, add input for prompt and path

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# bingImageGenerator
+
+# Bing Image Generator
+Bing Image Generator is a simple command-line tool that lets you generate images from a given text. It uses the [Bimg](https://github.com/nociza/Bimg) library to create images from the supplied text.
+
+## Project installation
+Before using the Bing image generator, make sure you have Node.js installed on your system. You can download it from [Node.org](https://nodejs.org/fr).
+
+Then run the command below to install the dependencies:
+
+`
+npm install
+`
+
+## Using the project
+Run the following command to launch the generator:
+
+`
+npm start
+`
+
+You will be prompted to enter the prompt you want to generate the image. 
+You will then be prompted to enter the name of the generated image.
+
+## Example
+Here's an example of how to use the Bing image generator:
+
+```
+npm start
+
+Enter text: A lion holding a book in its paws
+Enter image name: lion_image
+
+```
+
+License
+This project is licensed under the MIT license - see the LICENSE file for details.
+
+## 🔗 Liens
+[![portfolio](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://www.tmauc.fr/thomas)
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/tmauc/)
+[![twitter](https://img.shields.io/badge/twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/MaucSama)
+
---
+++ index.mjs
@@ -0,0 +1,61 @@
+import { generateImageFiles } from 'bimg';
+import sharp from 'sharp';
+import readline from 'readline';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const getUserInput = (question) => {
+  return new Promise((resolve) => {
+    rl.question(question, (answer) => {
+      resolve(answer);
+    });
+  });
+};
+
+const main = async () => {
+  const userInput = await getUserInput("Entrez une chaîne de texte : ");
+  const outputName = await getUserInput("Entrez le nom de l'image : ");
+
+  try {
+    const imageFiles = await generateImageFiles(userInput);
+    const base64ImageData = imageFiles[0].data;
+
+    const cropbase64Image = async (base64ImageData) => {
+      try {
+        const imageBuffer = Buffer.from(base64ImageData, 'base64');
+
+        const imageMetadata = await sharp(imageBuffer).metadata();
+        const originalWidth = imageMetadata.width;
+        const originalHeight = imageMetadata.height;
+
+        const left = 40;
+        const top = 40;
+        const newWidth = originalWidth - 80;
+        const newHeight = originalHeight - 80;
+
+        await sharp(imageBuffer).extract({
+          left,
+          top,
+          width: newWidth,
+          height: newHeight,
+        })
+        .toFile('./imgs/' + outputName + '.jpeg');
+
+        console.log(`Image recadrée et enregistrée avec succès sous ${outputName}.jpeg`);
+      } catch (error) {
+        console.log('Erreur lors du recadrage de l\'image :', error);
+      }
+    };
+
+    await cropbase64Image(base64ImageData);
+  } catch (error) {
+    console.log('Erreur lors de la génération des fichiers image :', error);
+  } finally {
+    rl.close();
+  }
+};
+
+main();
